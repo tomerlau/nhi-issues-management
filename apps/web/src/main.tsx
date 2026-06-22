e1e770bc8ef546f63fe36ba354c225032e1db6cb
Tomer Laufer <laufer.tomer@gmail.com>
2026-06-22 11:17:49 +0300

    fix

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
@@ -8,8 +7,4 @@ if (!rootElement) {
   throw new Error('Root element #root was not found in the document.');
 }
 
-createRoot(rootElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+createRoot(rootElement).render(<App />);
